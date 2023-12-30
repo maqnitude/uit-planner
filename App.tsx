f0d40dd394f9661e10ea0fabb9ab5502d20c4713
Tran Xuan Thanh <thanhthcsab@gmail.com>
2023-12-30 15:52:40 +0700

    Add course input screen

--- App.tsx
+++ App.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import CoursesScreen from './src/screens/CoursesScreen';
+import AddCourseScreen from './src/screens/AddCourseScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#1e90ff',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}>
         <Stack.Screen name="Courses" component={CoursesScreen} />
+        <Stack.Screen name="Add Course" component={AddCourseScreen} />
         {/* Other screens */}
       </Stack.Navigator>
     </NavigationContainer>
