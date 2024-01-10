188535ad5faadbc8fabd4a23296c1ad1faaa79c7
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-01-10 11:57:08 +0700

    handle time for timetable screen

--- src/components/CourseBlock.tsx
+++ src/components/CourseBlock.tsx
@@ -3,10 +3,10 @@ import { View, Text } from 'react-native';
 
 const CourseBlock = ({ course, color }) => {
   return (
-    <View style={{ flex: 1, padding: 10, backgroundColor: color }}>
-      <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{course.name}</Text>
-      <Text style={{ fontSize: 14, fontWeight: '300' }}>{course.code}</Text>
-      <Text style={{ fontSize: 14 }}>{course.location}</Text>
+    <View style={{ padding: 5, backgroundColor: color, height: 150, position: 'absolute', width: '100%' }}>
+      <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{course.name}</Text>
+      <Text style={{ fontSize: 12, fontWeight: '300' }}>{course.code}</Text>
+      <Text style={{ fontSize: 13 }}>{course.location}</Text>
     </View>
   );
 };
--- src/screens/TimetableScreen.tsx
+++ src/screens/TimetableScreen.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, View, Text, Dimensions } from 'react-native';
+import moment from 'moment';
+
 import CourseBlock from '../components/CourseBlock';
+import { Course } from '../types';
+import { getAllCourses } from '../storage/CoursesStorage';
 
 const TimeTable = () => {
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const times = ['7:30', '8:15', '9:00', '9:45', '10:45', '11:30', '13:00', '13:45', '14:30', '15:15', '16:15', '17:00'];
+  let timeStamps = ['7:30', '8:15', '9:00', '9:45', '10:45', '11:30', '13:00', '13:45', '14:30', '15:15', '16:15', '17:00'];
+  const times = timeStamps.map(time => moment(time, 'HH:mm'));
   const dayColors = {
     'Monday': '#CBE4F9',
     'Tuesday': '#CDF5F6',
@@ -13,53 +18,19 @@ const TimeTable = () => {
     'Friday': 'F9D8D6',
     'Saturday': '#D6CDEA',
   };
-  // create a list of dummy courses
-  const courses = [
-    {
-      name: "Machine Learning",
-      code: 'CS114',
-      credits: 4,
-      location: 'B1.02',
-      time: '7:30',
-      day: 'Monday',
-    },
-    {
-      name: "Computational Thinking",
-      code: 'CS117',
-      credits: 3,
-      location: 'B2.02',
-      time: '13:00',
-      day: 'Thursday',
-    },
-    {
-      name: "Algebra",
-      code: 'MA003',
-      credits: 4,
-      location: 'C4.02',
-      time: '10:30',
-      day: 'Wednesday',
-    },
-    {
-      name: "Homographic",
-      code: 'HS003',
-      credits: 3,
-      location: 'C4.08',
-      time: '9:45',
-      day: "Saturday",
-    },
-    {
-      name: "AblacaAblacaAblacaAblacaAblacaAblacaAblacaAblacaAblacaAblacaAblacaAblacaAblaca",
-      code: 'HS003',
-      credits: 3,
-      location: 'C4.0888888888888888888888888888888888888888',
-      time: '9:45',
-      day: "Wednesday",
-    },
-  ];
+
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const fetchedCourses = await getAllCourses();
+      setCourses(fetchedCourses ?? []);
+    };
+    fetchCourses();
+  }, []);
 
   const screenWidth = Dimensions.get('window').width;
-
-  const timeStampWidth = 50;
+  const timeStampWidth = 45;
   const headerHeight = 30;
   const blockHeight = 100;
 
@@ -68,7 +39,7 @@ const TimeTable = () => {
       <ScrollView>
         <View style={{ flexDirection: 'row' }}>
           <View style={{ marginBottom: blockHeight, marginTop: headerHeight }}>
-            {times.map((time, index) => (
+            {timeStamps.map((time, index) => (
               <View key={index} style={{ width: timeStampWidth, height: blockHeight, justifyContent: 'flex-start', borderBottomWidth: 0.5, borderTopWidth: index === 0 ? 0.5 : 0 }}>
                 <Text style={{ fontWeight: 'bold' }}>{time}</Text>
               </View>
@@ -79,13 +50,20 @@ const TimeTable = () => {
               {days.map((day, index) => (
                 <View key={index} style={{ width: (screenWidth - timeStampWidth) / 3 }}>
                   <Text style={{ fontWeight: 'bold', height: headerHeight, textAlign: 'center', backgroundColor: '#b5e2ff', borderLeftWidth: 0.5, borderBottomWidth: 0.5 }}>{day}</Text>
-                  {times.map((time, index) => (
-                    <View key={index} style={{ height: blockHeight, borderLeftWidth: 0.5, borderBottomWidth: 0.5 }}>
-                      {courses.map((course, index) => (
-                        course.time === time && course.day === day && <CourseBlock key={index} course={course} color={dayColors[day]} />
-                      ))}
-                    </View>
-                  ))}
+                  {times.map((time, index) => {
+                    return <View key={index} style={{ height: blockHeight, borderLeftWidth: 0.5, borderBottomWidth: 0.5 }}>
+                      {courses.map((course, index) => {
+                        const courseStartTime = moment(course.schedule[0].startTime).format('HH:mm');
+                        const formattedTime = time.format('HH:mm');
+                        console.log(`${courseStartTime} and ${formattedTime}`);
+                        return (
+                          courseStartTime === formattedTime &&
+                          course.schedule[0].day === day &&
+                          <CourseBlock key={index} course={course} color={dayColors[day]} />
+                        );
+                      })}
+                    </View>;
+                  })}
                 </View>
               ))}
             </View>
