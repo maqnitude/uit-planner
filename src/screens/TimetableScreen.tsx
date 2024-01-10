07eafcccb3aa6f0975014150110e6830503bb9e5
Tran Xuan Thanh <thanhthcsab@gmail.com>
2024-01-10 18:01:08 +0700

    render timetable from courses

--- src/screens/TimetableScreen.tsx
+++ src/screens/TimetableScreen.tsx
@@ -8,14 +8,14 @@ import { getAllCourses } from '../storage/CoursesStorage';
 
 const TimeTable = () => {
   const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  let timeStamps = ['7:30', '8:15', '9:00', '9:45', '10:45', '11:30', '13:00', '13:45', '14:30', '15:15', '16:15', '17:00'];
+  const timeStamps = ['7:30', '8:15', '9:00', '9:45', '10:30', '11:15', '12:00', '12:45', '13:00', '13:45', '14:30', '15:15', '16:15', '17:00'];
   const times = timeStamps.map(time => moment(time, 'HH:mm'));
   const dayColors = {
     'Monday': '#CBE4F9',
     'Tuesday': '#CDF5F6',
     'Wednesday': '#EFF9DA',
     'Thursday': '#F9EBDF',
-    'Friday': 'F9D8D6',
+    'Friday': '#F9D8D6',
     'Saturday': '#D6CDEA',
   };
 
@@ -30,37 +30,40 @@ const TimeTable = () => {
   }, []);
 
   const screenWidth = Dimensions.get('window').width;
-  const timeStampWidth = 45;
+  const timeStampWidth = 50;
   const headerHeight = 30;
-  const blockHeight = 100;
-
+  const unitHeight = 90;
   return (
     <View style={{ flex: 1, flexDirection: 'row' }}>
       <ScrollView>
         <View style={{ flexDirection: 'row' }}>
-          <View style={{ marginBottom: blockHeight, marginTop: headerHeight }}>
+          <View style={{ marginBottom: unitHeight, marginTop: headerHeight }}>
             {timeStamps.map((time, index) => (
-              <View key={index} style={{ width: timeStampWidth, height: blockHeight, justifyContent: 'flex-start', borderBottomWidth: 0.5, borderTopWidth: index === 0 ? 0.5 : 0 }}>
+              <View key={index} style={{ width: timeStampWidth, height: unitHeight, justifyContent: 'flex-start', alignItems: 'center', borderBottomWidth: 0.5, borderRightWidth: 0.5, borderTopWidth: index === 0 ? 0.5 : 0 }}>
                 <Text style={{ fontWeight: 'bold' }}>{time}</Text>
               </View>
             ))}
           </View>
           <ScrollView horizontal>
             <View style={{ flexDirection: 'row' }}>
-              {days.map((day, index) => (
-                <View key={index} style={{ width: (screenWidth - timeStampWidth) / 3 }}>
+              {days.map((day, dayIndex) => (
+                <View key={dayIndex} style={{ width: (screenWidth - timeStampWidth) / 3 }}>
                   <Text style={{ fontWeight: 'bold', height: headerHeight, textAlign: 'center', backgroundColor: '#b5e2ff', borderLeftWidth: 0.5, borderBottomWidth: 0.5 }}>{day}</Text>
-                  {times.map((time, index) => {
-                    return <View key={index} style={{ height: blockHeight, borderLeftWidth: 0.5, borderBottomWidth: 0.5 }}>
-                      {courses.map((course, index) => {
+                  {times.map((time, timeIndex) => {
+                    return <View key={timeIndex} style={{ height: unitHeight, borderRightWidth: 0.5, borderBottomWidth: 0.5 }}>
+                      {courses.map((course, courseIndex) => {
                         const courseStartTime = moment(course.schedule[0].startTime).format('HH:mm');
-                        const formattedTime = time.format('HH:mm');
-                        console.log(`${courseStartTime} and ${formattedTime}`);
-                        return (
-                          courseStartTime === formattedTime &&
-                          course.schedule[0].day === day &&
-                          <CourseBlock key={index} course={course} color={dayColors[day]} />
-                        );
+                        const courseEndTime = moment(course.schedule[0].endTime).format('HH:mm');
+                        const timeSlotEnd = moment(time).add(45, 'minutes').format('HH:mm');
+                        if (courseStartTime >= time.format('HH:mm') && courseStartTime < timeSlotEnd && course.schedule[0].day === day) {
+                          const courseDurationMinutes = moment.duration(moment(courseEndTime, 'HH:mm').diff(moment(courseStartTime, 'HH:mm'))).asMinutes();
+                          const blockHeight = courseDurationMinutes * unitHeight / 45;
+                          const offsetMinutes = moment.duration(moment(courseStartTime, 'HH:mm').diff(moment(time, 'HH:mm'))).asMinutes();
+                          const offset = offsetMinutes * unitHeight / 45;
+                          console.log(`minutes: ${courseDurationMinutes}`);
+                          console.log(blockHeight);
+                          return <CourseBlock key={courseIndex} course={course} color={dayColors[day]} height={blockHeight} offset={offset} />;
+                        }
                       })}
                     </View>;
                   })}
