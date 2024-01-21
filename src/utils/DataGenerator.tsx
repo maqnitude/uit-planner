9f20a2960c9bead4b277fef1e28649259b835ddc
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-01-22 00:31:13 +0700

    courses generation: separate into morning and afternoon

--- src/utils/DataGenerator.tsx
+++ src/utils/DataGenerator.tsx
@@ -18,24 +18,30 @@ export const generateSemesters = (num_semesters: number = 1): Semester[] => {
   return semesters;
 };
 
-const timeStamps = ['7:30', '8:15', '9:00', '10:00', '10:45', '13:00', '13:45', '14:30', '15:15'];
+const morningTimeStamps = ['7:30', '8:15', '9:00', '10:00'];
+const afternoonTimeStamps = ['13:00', '13:45', '14:30', '15:30'];
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const courseCodePrefix = ['CS', 'SE'];
-const coursePreiods = [2, 3, 4, 5];
+const coursePeriods = [2, 3, 4, 5];
 const courseCredits = [2, 3, 4, 10];
 const courseLocationPrefix = ['B', 'C'];
+const MAX_COURSES_PER_DAY = 3;
 
 export const generateCourses = (semesters: Semester[], num_courses: number = 6): Course[] => {
   const courses: Course[] = [];
-  const daySchedules: {[key: string]: string[]} = {};
+  const morningSchedules: {[key: string]: string[]} = {};
+  const afternoonSchedules: {[key: string]: string[]} = {};
   const courseCountPerDay: {[key: string]: number} = {};
-  const latestEndTime: {[key: string]: Date} = {};
+  const latestEndTimeMorning: {[key: string]: Date} = {};
+  const latestEndTimeAfternoon: {[key: string]: Date} = {};
 
   semesters.forEach(semester => {
     days.forEach(day => {
-      daySchedules[day] = [...timeStamps];
+      morningSchedules[day] = [...morningTimeStamps];
+      afternoonSchedules[day] = [...afternoonTimeStamps];
       courseCountPerDay[day] = 0;
-      latestEndTime[day] = new Date(`1970-01-01T${timeStamps[0]}`);
+      latestEndTimeMorning[day] = new Date(`1970-01-01T${morningTimeStamps[0]}`);
+      latestEndTimeAfternoon[day] = new Date(`1907-01-01T${afternoonTimeStamps[0]}`)
     });
 
     for (let i = 0; i < num_courses; i++) {
@@ -52,19 +58,34 @@ export const generateCourses = (semesters: Semester[], num_courses: number = 6):
       let day: string;
       do {
         day = days[Math.floor(Math.random() * days.length)];
-      } while (courseCountPerDay[day] >= 2 || daySchedules[day].length === 0);
+      } while (courseCountPerDay[day] >= MAX_COURSES_PER_DAY || (morningSchedules[day].length === 0 || afternoonSchedules[day].length === 0));
 
       let startTimeStamp: string;
       let startTime: Date;
-      do {
-        startTimeStamp = daySchedules[day].splice(Math.floor(Math.random() * daySchedules[day].length), 1)[0];
-        startTime = new Date(`1970-01-01T${startTimeStamp}:00`);
-      } while (startTime < latestEndTime[day]);
 
+      if (morningSchedules[day].some(timeStamp => new Date(`1970-01-01T${timeStamp}:00`) >= latestEndTimeMorning[day])) {
+        do {
+          startTimeStamp = morningSchedules[day].splice(Math.floor(Math.random() * morningSchedules[day].length), 1)[0];
+          startTime = new Date(`1970-01-01T${startTimeStamp}:00`);
+        } while (startTime < latestEndTimeMorning[day]);
+      } else if (afternoonSchedules[day].some(timeStamp => new Date(`1970-01-01T${timeStamp}:00`) >= latestEndTimeAfternoon[day])) {
+        do {
+          startTimeStamp = afternoonSchedules[day].splice(Math.floor(Math.random() * afternoonSchedules[day].length), 1)[0];
+          startTime = new Date(`1970-01-01T${startTimeStamp}:00`);
+        } while (startTime < latestEndTimeAfternoon[day]);
+      } else {
+        console.log(`No more valid time slots for ${day}.`)
+        continue;
+      }
 
-      const num_periods = coursePreiods[Math.floor(Math.random() * coursePreiods.length)];
+      let num_periods = coursePeriods[Math.floor(Math.random() * coursePeriods.length)];
       let endTime = new Date(startTime.getTime() + 45 * num_periods * 60 * 1000);
-      if (endTime.getHours() > 17 || (endTime.getHours() === 17 && endTime.getMinutes() > 45)) {
+      let endTimeHours = endTime.getHours();
+      let endTimeMinutes = endTime.getMinutes();
+
+      if ((endTimeHours === 11 && endTimeMinutes > 30) || (endTimeHours ===12) ||  (endTimeHours === 13)) {
+        endTime = new Date('1970-01-01T11:30:00');
+      } else if (endTimeHours > 17 || (endTimeHours === 17 && endTimeMinutes > 45)) {
         endTime = new Date('1970-01-01T17:45:00');
       }
 
@@ -78,7 +99,11 @@ export const generateCourses = (semesters: Semester[], num_courses: number = 6):
       courses.push(course);
 
       courseCountPerDay[day]++;
-      latestEndTime[day] = endTime;
+      if (endTime.getHours() < 12) {
+        latestEndTimeMorning[day] = endTime;
+      } else {
+        latestEndTimeAfternoon[day] = endTime;
+      }
     }
   });
 
