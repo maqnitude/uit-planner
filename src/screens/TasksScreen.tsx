cbfcd7bec3b506125695ede0f94c575991b93a55
Jun0se7en <jun0se7en115@gmail.com>
2024-01-23 21:44:25 +0700

    Resolve Conflicts on TasksScreen

--- src/screens/TasksScreen.tsx
+++ src/screens/TasksScreen.tsx
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SectionList, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 import { useFocusEffect } from '@react-navigation/native';
 import 'react-native-get-random-values';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import moment from 'moment';
 
 import { Task } from '../types';
-import { getAllTasks, removeTask } from '../storage/TasksStorage';
+import { getAllTasks, updateTask } from '../storage/TasksStorage';
 import { deleteTask } from '../utils/TaskManager';
 import { useCurrentSemester } from '../hooks/CurrentSemesterContext';
 import { getAllCourses } from '../storage/CoursesStorage';
-
-import SearchBar from '../components/SearchBars';
 
 interface TasksScreenProps {
   navigation: any;
 }
 
+interface Section {
+  title: string;
+  data: Task[];
+}
+
 const TasksScreen: React.FC<TasksScreenProps> = ({ navigation }) => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [sections, setSections] = useState<Section[]>([]);
   const { currentSemesterId } = useCurrentSemester();
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-
-  const [selectedTasks, setSelectedTasks] = useState(null);
 
   const fetchTasks = React.useCallback(async () => {
     setIsLoading(true);
@@ -34,10 +36,16 @@ const TasksScreen: React.FC<TasksScreenProps> = ({ navigation }) => {
       // get tasks by semester id
       const fetchedTasks = await getAllTasks();
       const courses = await getAllCourses();
-      const currentSemesterCourseIds = courses?.filter(course => course.semesterId === currentSemesterId).map(course => course.id) ?? [];
-      const currentSemesterTasks = fetchedTasks?.filter(task => currentSemesterCourseIds.includes(task.courseId)) ?? [];
+      const currentSemesterCourses = courses?.filter(course => course.semesterId === currentSemesterId) ?? [];
+      const currentSemesterTasks = fetchedTasks?.filter(task => currentSemesterCourses.some(course => course.id === task.courseId)) ?? [];
 
-      setTasks(currentSemesterTasks);
+      // group tasks by course and sort within each course
+      const groupedTasks = currentSemesterCourses.map(course => ({
+        title: `${course.code} - ${course.name}`,
+        data: currentSemesterTasks.filter(task => task.courseId === course.id).sort((a, b) => Number(a.completed) - Number(b.completed)),
+      }));
+
+      setSections(groupedTasks);
     } catch (err) {
       setError((err as Error).message);
     } finally {
@@ -55,6 +63,19 @@ const TasksScreen: React.FC<TasksScreenProps> = ({ navigation }) => {
     navigation.navigate('Task Details', { item });
   };
 
+  const handleToggleComplete = async (item: Task, newValue: boolean) => {
+    item.completed = newValue;
+    await updateTask(item);
+
+    setSections(sections.map(section => ({
+      ...section,
+      data: section.data.map(task => task.id === item.id ? { ...task, completed: newValue } : task),
+    })).map(section => ({
+      ...section,
+      data: section.data.sort((a, b) => Number(a.completed) - Number(b.completed)),
+    })));
+  };
+
   const handleDeletePress = async (item: Task) => {
     Alert.alert(
       'Delete Task',
@@ -67,7 +88,6 @@ const TasksScreen: React.FC<TasksScreenProps> = ({ navigation }) => {
         {
           text: 'OK',
           onPress: async () => {
-            // await removeTask(item.id);
             await deleteTask(item);
             fetchTasks();
           },
@@ -76,37 +96,37 @@ const TasksScreen: React.FC<TasksScreenProps> = ({ navigation }) => {
     );
   };
 
-  const handleSearch = (searchQuery, tasks) => {
-    setSelectedTasks(tasks.filter(task => task.name.includes(searchQuery.trim())));
-  };
-
-  useEffect(() => {
-    setSelectedTasks(tasks);
-  }, [isLoading, error]);
-
   return (
     <View style={styles.container}>
       {isLoading && <Text>Loading tasks...</Text>}
       {error && <Text style={styles.errorText}>Error loading tasks: {error}</Text>}
-      <SearchBar
-        onSearch={(searchQuery) => handleSearch(searchQuery,tasks)}
-      />
+
       {!isLoading && !error && (
-        <FlatList
+        <SectionList
           contentContainerStyle={styles.listContent}
-          data={selectedTasks}
+          sections={sections}
           renderItem={({ item }) => (
-            <View style={styles.itemBlock}>
-              <TouchableOpacity onPress={() => handleItemPress(item)}>
-                <View>
-                  <Text style={styles.itemTitle}>{item.name}</Text>
-                  <Text style={styles.itemDue}>Due: {moment(item.dueDate).format('HH:mm:ss DD/MM/YYYY')}</Text>
-                </View>
-              </TouchableOpacity>
+            <View style={[styles.itemBlock, item.completed ? styles.completedTaskBlock : {}]}>
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                  value={item.completed}
+                  onValueChange={(newValue) => handleToggleComplete(item, newValue)}
+                />
+                <TouchableOpacity style={styles.itemDetails} onPress={() => handleItemPress(item)}>
+                  <View>
+                    <Text style={[styles.itemTitle, item.completed ? styles.completedTaskTitle : {}]}>{item.name}</Text>
+                    <Text style={[styles.itemStatus, item.completed ? styles.completedTaskStatus : {}]}>{item.completed ? 'DONE' : 'TODO'}</Text>
+                    <Text style={[styles.itemDue, item.completed ? styles.completedTaskTitle : {}]}>Due: {moment(item.dueDate).format('HH:mm:ss DD/MM/YYYY')}</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
               <TouchableOpacity onPress={() => handleDeletePress(item)}>
                 <Icon name="delete" size={25} />
               </TouchableOpacity>
             </View>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
           )}
           keyExtractor={item => item.name}
         />
@@ -125,6 +145,16 @@ const styles = StyleSheet.create({
   listContent: {
     paddingBottom: 70,
   },
+  header: {
+    color: '#003399',
+    fontSize: 20,
+    fontWeight: 'bold',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    marginTop: 20,
+    marginHorizontal: 20,
+    padding: 5,
+  },
   itemBlock: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -133,17 +163,41 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginTop: 20,
     marginHorizontal: 20,
-    padding: 10,
+    padding: 5,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  itemDetails: {
+    marginLeft: 7,
   },
   itemTitle: {
-    fontSize: 20,
+    fontSize: 16,
     color: 'black',
     fontWeight: 'bold',
   },
+  itemStatus: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'green',
+    marginHorizontal: 2,
+  },
   itemDue: {
-    fontSize: 16,
-    color: 'blue',
-    margin: 2,
+    fontSize: 14,
+    color: '#900d09',
+    marginHorizontal: 2,
+  },
+  completedTaskBlock: {
+    backgroundColor: '#b5b5b5',
+  },
+  completedTaskTitle: {
+    color: 'black',
+    textDecorationStyle: 'solid',
+    textDecorationLine: 'line-through',
+  },
+  completedTaskStatus: {
+    color: '#1a98a6',
   },
 });
 
